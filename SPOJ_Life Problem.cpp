44d01d98a9b10d66f8ae2d37a72335b92d3d7024
Rahul Kumar Singh <singhrahulkumar805@gmail.com>
2019-06-12 07:57:41 +0530

    Create SPOJ_Life Problem.cpp

---
+++ SPOJ_Life Problem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0,n;
+    int a[i];
+    
+   
+   while(a[i]!=42)
+    {
+	cin>>a[i];
+	if(a[i]!=42)
+	cout<<a[i]<<"\n";
+
+    }
+   
+
+return 0;
+
+}
